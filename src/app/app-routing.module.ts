e81d788f2404ea5eac20e530977dcf91196ada14
sudip kumar prasad <sudiphansraj1@gmail.com>
2019-05-01 01:39:01 +0530

    error in data part corrected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import {AppAuthGuard} from './app.authguard';
 
 const routes: Routes = [
 {path:'', component:HomeComponent},
-{path:'about', component:AboutComponent,canActivate:[AppAuthGuard],data:{roles['SuperAdmin','storemanager']}},
-{path:'contact',component:ContactComponent,canActivate:[AppAuthGuard],data:{roles['SuperAdmin','storemanager']}}
+{path:'about', component:AboutComponent,canActivate:[AppAuthGuard],data:{roles:['SuperAdmin','storemanager']}},
+{path:'contact',component:ContactComponent,canActivate:[AppAuthGuard],data:{roles:['SuperAdmin','storemanager']}}
 
 ];
 
